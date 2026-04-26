4f8ab4f6e9bb69aa93cd8d79cb8edae1e295b965
prateekkumaryadav <prateek.yadav2006@gmail.com>
2026-04-26 12:02:22 +0530

    changing port to 5001 from 5000 as its used by mac services

--- server/server.js
+++ server/server.js
@@ -44,7 +44,7 @@ app.get('/', (req, res) => {
 });
 
 if (require.main === module) {
-  const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 5001;
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
